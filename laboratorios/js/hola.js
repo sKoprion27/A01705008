659ad04310305f67b5577e29d0f01605f935b963
julio <rafer2797@outlook.com>
2021-02-22 16:52:42 -0600

    funciones tradicionales

--- laboratorios/js/hola.js
+++ laboratorios/js/hola.js
@@ -5,6 +5,42 @@ console.info("el precio de los tacos es de $" + precio);
 
 let cantidad_tacos = 4;
 
-console.log("Te voy a mandar " + cantidad_tacos);
+console.log("Te voy a mandar " + cantidad_tacos + " tacos");
+
+let persona = "lalo";
+let descuento = 0;
+
+if (cantidad_tacos > 6 && persona === "lalo"){
+    descuento = 0.1 * (cantidad_tacos * precio);
+}
+
+
+const por_tacos = cantidad_tacos * precio - descuento;
+
+console.info("Por pagar : " + por_tacos);
+
+const clientes = [];
+
+clientes.push('lalo');
+clientes.push('javier');
+clientes.push('julio')
+
+for (let cliente of clientes){
+    console.log(clientes);
+}
+
+
+const menu = [];
+menu.push({nombre: 'El paisa', precio: 22});
+menu.push({nombre: 'El Tacoshino', precio: 29});
+
+console.log(menu);
+
+function calcular_cuenta(tacos){
+    return tacos*22;
+}
+
+console.log(' la cuenta es de : $' + calcular_cuenta(7) );
+
 
 
