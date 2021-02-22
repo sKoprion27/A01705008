a1c8c7fd4476e288038555e268a620fafbe7b52f
julio <rafer2797@outlook.com>
2021-02-22 16:27:24 -0600

    reestructuracion

---
+++ laboratorios/js/hola.js
@@ -0,0 +1,10 @@
+console.log("Hola, mundo");
+
+const precio = 22;
+console.info("el precio de los tacos es de $" + precio);
+
+let cantidad_tacos = 4;
+
+console.log("Te voy a mandar " + cantidad_tacos);
+
+
