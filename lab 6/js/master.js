3b4a896e820d1645eeb93c0a6a91f2458411d6fa
julio <rafer2797@outlook.com>
2021-02-25 17:43:45 -0600

    agregar ejemplos corregidos

--- lab 6/js/master.js
+++ lab 6/js/master.js
@@ -5,35 +5,48 @@ function myFunction() {
   document.getElementById("myList").appendChild(node);
 }
 
-function fav_piedras(){
-  document.getElementById("fav_piedras");
-  innerHTML = document.getElementById('c_piedras').innerHTML = '';
+function fav_piedras() {
+  document.getElementById("fav_piedras").innerHTML = document.getElementById('c_piedras').innerHTML;
+  document.getElementById('c_piedras').innerHTML = '';
   let boton = document.getElementById('btn_piedras');
-  boton.innerHTML = 'eliminar de favoritos';
-  boton.onclick= quitar_piedaras();
+  boton.innerHTML = 'Eliminar de favoritos';
+  boton.onclick = quitar_piedras;
 }
 
-function quitar_piedaras(){
-  document.getElementById("c_piedras");
-  inner = document.getElementById('fav_piedras').innerHTML = '';
+function quitar_piedras() {
+  document.getElementById("c_piedras").innerHTML = document.getElementById('fav_piedras').innerHTML;
+  document.getElementById('fav_piedras').innerHTML = '';
   let boton = document.getElementById('btn_piedras');
-  boton.innerHTML = 'agregar de favoritos';
-  boton.onclick= quitar_piedaras();
-
+  boton.innerHTML = 'Agregar a favoritos';
+  boton.onclick = fav_piedras;
 }
 
-
-function fav_like(){
-  let li = document.getElementById("fav_like");
-  innerHTML = document.getElementById('c_like').innerHTML;
-  li.onclick = quitar_like();
-  document.getElementById('c_like'),innerHTML ='';
+function fav_like() {
+  let list_item = document.getElementById("fav_like");
+  list_item.innerHTML = document.getElementById('c_like').innerHTML;
+  list_item.onclick = quitar_like;
+  document.getElementById('c_like').innerHTML = '';
 }
 
-function quitar_like(){
+function quitar_like() {
   let li = document.getElementById("c_like");
-  innerHTML = document.getElementById('fav_like').innerHTML;
-  li.onclick = fav_like();
-  document.getElementById('fav_like'),innerHTML ='';
+  li.innerHTML = document.getElementById('fav_like').innerHTML;
+  li.onclick = fav_like;
+  document.getElementById('fav_like').innerHTML = '';
+}
 
+document.getElementById('c_lobo').onclick = fav_lobo;
+
+function fav_lobo() {
+  let list_item = document.getElementById("fav_lobo");
+  list_item.innerHTML = document.getElementById('c_lobo').innerHTML;
+  list_item.onclick = quitar_lobo;
+  document.getElementById('c_lobo').innerHTML = '';
+}
+
+function quitar_lobo() {
+  let li = document.getElementById("c_lobo");
+  li.innerHTML = document.getElementById('fav_lobo').innerHTML;
+  li.onclick = fav_lobo;
+  document.getElementById('fav_lobo').innerHTML = '';
 }
