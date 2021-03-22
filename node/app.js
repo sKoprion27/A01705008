e36f340c47f0417dd4133874317652b12592a840
julio <rafer2797@outlook.com>
2021-03-22 16:55:37 -0600

    Agregar middlwware

--- node/app.js
+++ node/app.js
@@ -1,1 +1,22 @@
 console.log("hola mundo");
+
+const { response } = require('express');
+const express = require('express');
+const app = express();
+
+//Middleware
+app.use((request, response, next) => {
+    console.log('Middleware!');
+    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+app.use('/ruta', (request, response,next)=>{
+    response.send('Respuesta de la ruta "/ruta"');
+});
+
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+    response.send('¡Hola mundo!'); //Manda la respuesta
+});
+
+app.listen(3000);
