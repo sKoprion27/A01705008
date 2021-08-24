1211ac76214925e7996390472404f2eb46bdbfdb
Julio <rafer2797@outlook.com>
2021-08-23 21:42:27 -0500

    add exercise3

--- lab 5/main.js
+++ lab 5/main.js
@@ -5,7 +5,6 @@ function Entrada1() {
     if (num < 0) {
         alert("Introduce un número positivo");
     } else {
-        document.open();
         // genera el encabezado de la tabla
         document.write("<table> <tr> <th>número introducido   </th> <th>potencia</th> </tr> <tr>");
         for (let i = 0; i < num; i++) {
@@ -15,7 +14,6 @@ function Entrada1() {
             document.write("</tr> ");
         }
 
-        document.close();
     }
 }
 
@@ -40,9 +38,45 @@ function Entrada2() {
 }
 
 function Entrada3() {
+    var x = document.getElementById("Entrada3");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }    
+}
+
+// se genera el arreglo vacio como variable global
+var arr = [];
+
+function cuentaArreglos(){
     // Función: contador. Parámetros: Un arreglo de números. 
     // Regresa: La cantidad de números negativos en el arreglo, la cantidad de 0's, y la cantidad de valores mayores a 0 en el arreglo.
+    let positivos =0, negativos=0, ceros=0;
+    for(let i =0; i <= arr.length-1; i++){
+        // se cuentan los positivos, negativos y ceros
+        if (arr[i] == 0){
+            ceros++;
+        }else if (arr[i] > 0){
+            positivos++;
+        }else{
+            negativos++;
+        }
+    }
+    // se muestra el resultado
+    alert("Numero de Positivos: " + positivos + " Negativos: " + negativos + " Ceros: " + ceros)
+
 }
+
+
+function agregaItem(item){
+    // se agrega el item al arreglo arr
+    arr.push(item);
+    // se agrega el item al div
+    document.getElementById("mydiv").innerHTML += item + " " 
+}
+
+
 
 function Entrada4() {
     // Función: promedios. Parámetros: Un arreglo de arreglos de números. 
