05e406459b91d52d7925b207d4f89c5ff8fbe4be
Julio <rafer2797@outlook.com>
2021-08-26 17:01:37 -0500

    add front-end

---
+++ lab6/static/js/master.js
@@ -0,0 +1,11 @@
+function equivalenciaPassword(password1, password2){
+    console.log("contraseña " + password1)
+    console.log("Ccontraseña " + password2)
+    console.log("-----------------------------------")
+    
+    if(password1 === password2){
+        console.log("son iguales")
+    }else{
+        console.log("no son iguales")
+    }
+}
