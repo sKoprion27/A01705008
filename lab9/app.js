69ea0fe32e13b06aa21cdc003bf14735a5db390d
Julio <rafer2797@outlook.com>
2021-09-06 17:26:43 -0500

    procesar los datos del cliente

--- lab9/app.js
+++ lab9/app.js
@@ -17,8 +17,14 @@
 
 const http = require('http');
 
+const platillos = [
+    {nombre:"Sopes", descripcion:"Tortilla, frijol, queso, salsa, pollo"},
+    {nombre:"Chilaquiles", descripcion:"Tortilla, salsa, frijor, queso, pollo"},
+    {nombre:"Tacos", descripcion:"Tortilla, carne, salsa"},
+    {nombre:"pambazo", descripcion:"Bolillo remojado en salsa con carne"},
+]
 const server = http.createServer((request, response) => {
-
+    
     // reaccionar de acuerdo a la ruta
     if (request.url === "/hola"){
         response.setHeader('Content-Type', 'text/html');
@@ -27,12 +33,6 @@ const server = http.createServer((request, response) => {
         response.end();
     }else if (request.url === "/menu"){
         // mostrar lista de platillos
-        const platillos = [
-            {nombre:"Sopes", descripcion:"Tortilla, frijol, queso, salsa, pollo"},
-            {nombre:"Chilaquiles", descripcion:"Tortilla, salsa, frijor, queso, pollo"},
-            {nombre:"Tacos", descripcion:"Tortilla, carne, salsa"},
-            {nombre:"pambazo", descripcion:"Bolillo remojado en salsa con carne"},
-        ]
         response.setHeader('Content-Type', 'text/html');
         response.write('<head><meta charset="UTF-8"></head>');
         response.write('<h1>Menú</h1>');
@@ -43,7 +43,7 @@ const server = http.createServer((request, response) => {
         response.write("</ul>");
         response.write('<a href="/menu/add">Agregar platillo </a>');
         response.end();
-    }else if(request.url === "/menu/add"){
+    }else if(request.url === "/menu/add" && request.method === "GET"){
         response.setHeader('Content-Type', 'text/html');
         response.write('<head><meta charset="UTF-8"></head>');
         response.write('<h1>Agregar platillo al menú</h1>');
@@ -54,14 +54,39 @@ const server = http.createServer((request, response) => {
         response.write('<br>');
         response.write('<label for="descripcion">Descripcion del platillo</label>');
         response.write('<input type="text" id="descripcion" name="descripcion" placeholder="Tortilla, carne, salsa" required>');
+        response.write('<br>');
+        response.write('<br>');
         response.write('<input type="submit" id="enviar" name="name" value="Enviar">');
-        response.write('<br>');
-        response.write('<br>');
         response.write('</form>');
         response.end();
+    }else if(request.url === "/menu/add" && request.method === "POST"){
+        const datos =  [];
+        // recibir datos del cliente
+        request.on('data', (dato) => {
+            // console.log(dato);
+            datos.push(dato);
+        });
+        // console.log(datos);
 
-    }
-    else{
+        // procesar datos del cliente
+        return request.on('end', () => {
+            const datos_completos = Buffer.concat(datos).toString();
+            // console.log(datos);
+            // console.log(datos_completos);
+            const nombre = datos_completos.split('=')[1].split('&')[0]; 
+            const descripcion = datos_completos.split('=')[2].split('&')[0]; 
+            console.log(nombre);
+            console.log(descripcion);
+            // agregar el nuevo platillo
+            platillos.push({nombre: nombre, descripcion: descripcion});
+            // redireccionar a menu
+            response.statusCode = 303
+            response.setHeader('Location', '/menu');
+            response.end();
+        })
+
+
+    }else{
         response.statusCode = 404;
         response.setHeader('Content-Type', 'text/html');
         response.write('<head><meta charset="UTF-8"></head>');
