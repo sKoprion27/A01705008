a3fc724b9d0144959085e69ad5323d367d48602c
Julio <rafer2797@outlook.com>
2021-09-20 17:22:34 -0500

    rutas2

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 //Middleware
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((request, response, next) => {
     console.log('Primer Middleware!');
     next();
 } );
 
-app.use('/ruta/nombres', (request, response, next) => {
-    response.send('Respuesta de la ruta "/ruta/nombres"'); 
-    next();
+app.get('/menu/add', (request, response, next) => {
+    let respuesta = '<head><meta charset="UTF-8"></head>';
+        respuesta +='<h1>Agregar platillo al menú</h1>';
+        respuesta +='<form action="/menu/add" method="POST">';
+        respuesta +='<label for="nombre">Nombre del platillo</label>';
+        respuesta +='<input type="text" id="nombre" name="nombre" placeholder="Tacos" required>';
+        respuesta +='<br>';
+        respuesta +='<br>';
+        respuesta +='<label for="descripcion">Descripcion del platillo</label>';
+        respuesta +='<input type="text" id="descripcion" name="descripcion" placeholder="Tortilla, carne, salsa" required>';
+        respuesta +='<br>';
+        respuesta +='<br>';
+        respuesta +='<input type="submit" id="enviar" name="name" value="Enviar">';
+        respuesta +='</form>';
+        response.send(respuesta);
+});
+
+app.post('/menu/add', (request, response, next) => {
+    console.log(request.body);
+    console.log(request.body.nombre);
+    response.send("gracias");
 });
 
 app.use('/ruta/apellidos', (request, response, next) => {
