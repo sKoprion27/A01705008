11859dfd721f81bedca97f748206e0cdb4d4d026
Julio <rafer2797@outlook.com>
2021-09-25 16:22:37 -0500

    mostrar maestros disponibles en tamplate addMaestre

--- routes/lab14.js
+++ routes/lab14.js
@@ -2,6 +2,9 @@ const express = require('express');
 const lab14 = express.Router();
 
 
+// arreglos
+var Maestros = [];
+
 // rutas
 lab14.get('/', (request, response, next) => {
     response.render('homepagelab14', {
@@ -25,17 +28,21 @@ lab14.get('/addMateria', (request, response, next) => {
 
 lab14.route('/addMaestre')
     .get(function (request, response) {
+        path = 'public/txt/Maestros.txt';
+        CrearArr(path)
         response.render('addMaestre', {
-            Titulo: "Agregar Maestre"
+            Titulo: "Agregar Maestre",
+            Lista_maestros: Maestros
         });
     })
     .post(function (request, response){
         path = 'public/txt/Maestros.txt';
         agregarDatoTxt(request.body.nombreMaestro, path)
         // console.log(request.body.nombreMaestro)
-        response.send("llega al post")
-    })
+        response.send("Maestre agregado")
 
+    })
+ 
 
 lab14.get('/listadoTareas', (request, response, next) => {
     response.render('listadoTareas', {
@@ -47,16 +54,29 @@ lab14.get('/listadoTareas', (request, response, next) => {
 // fuciones
 
 
-const fs = require('fs')
+const fs = require('fs');
 
 // funciones para lab 14
 function agregarDatoTxt(content, path){
     console.log(content);
-    fs.appendFile(path, content + "\n" , function (err) {
+    // abre el archivo ubicado en path, y agrega el contenico con un \n al inicio para hacer salto de linea
+    fs.appendFile(path,"\n"  + content, function (err) {
     if (err) return console.log(err);
     console.log(content + " agregado a " + path);
     });
 
+}
+
+function CrearArr(path){
+    // console.log("entra")
+    fs.readFile(path, 'utf8' , (err, data) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        Maestros = Array.from(data.split("\n"));
+        console.log(Maestros)
+      })
 }
 
 module.exports = lab14
