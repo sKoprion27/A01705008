3abe045f32315ff261348bf15af5753af2e72c2c
Julio <rafer2797@outlook.com>
2021-10-07 16:12:06 -0500

    add controller.js

--- controllers/menu_controller.js
+++ controllers/menu_controller.js
@@ -1,22 +1,27 @@
 const Platillo = require("../models/platillos");
 
-exports.getlist = (request, response, next) => {
+exports.getList = (request, response, next) => {
+    console.log(request.get('Cookie'));
+    console.log(request.get('Cookie').split(';')[2].trim().split('=')[1]);
     response.render('lista_menu', {
         Titulo: "Menú",
         lista_platillos: Platillo.fetchAll()
     });
 };
 
-exports.getadd = (request, response, next) => {
+
+
+exports.getAdd = (request, response, next) => {
     response.render('addMenu', {
         Titulo: "Agrgar Platillo ",
     });
         
 };
 
-exports.postadd = (request, response, next) => {
+exports.postAdd = (request, response, next) => {
     // console.log(request.body);
-    // console.log(request.body.nombre);
+    console.log(request.body.nombre);
+    response.setHeader('Set-Cookie', 'ultimo_platillo='+request.body.nombre);
     const p = new Platillo(request.body.nombre, request.body.descripcion, "https://i.blogs.es/87930e/comidas-ricas/650_1200.jpg");
     Platillo.save();
     response.status(302).redirect('/menu/list')
--- routes/menu.js
+++ routes/menu.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 const menuControlller = require('../controllers/menu_controller')
 
-router.get('/list',  menuControlller.getlist);
+router.get('/list',  menuControlller.getList);
 
-router.get('/add', menuControlller.getadd);
+router.get('/add', menuControlller.getAdd);
 
-router.post('/add', menuControlller.postadd);
+router.post('/add', menuControlller.postAdd);
 
 module.exports = router;
